78d5307188f51b78a5c56947395edcf6ceb525ba
ticobrahe <olabiyi.sam@gmail.com>
2019-07-17 01:48:56 +0100

    update package.json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import route from './routes/index';
 import helper from './controllers/helper';
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
